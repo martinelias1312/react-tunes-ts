71af81fa8cf0f86f444b2824984cd2b0b1ae6bc5
smradupan <martinelias630@gmail.com>
2022-04-30 14:12:02 +0200

    comment added

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
 import { BrowserRouter } from "react-router-dom";
 
+// styles
 import "./index.scss";
 import App from "./App";
 
