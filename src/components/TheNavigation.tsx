a2c922aa103a11132b8a539e92923468a289d21d
smradupan <martinelias630@gmail.com>
2022-04-30 14:30:48 +0200

    navigation styles added

--- src/components/TheNavigation.tsx
+++ src/components/TheNavigation.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
+// styles
+import styles from "./TheNavigation.module.scss";
+
 type Props = {};
 
 const TheNavigation = (props: Props) => {
   return (
-    <nav className="navigation">
+    <nav className={styles.navigation}>
       <NavLink to="/">Home</NavLink>
       <NavLink to="/tunes">Tunes</NavLink>
       <NavLink to="/about">About</NavLink>
