c9634b5e0daa7682d9341e4b66f0c58bc59a3cd4
smradupan <martinelias630@gmail.com>
2022-04-30 14:12:21 +0200

    about + styles added

--- src/views/About.tsx
+++ src/views/About.tsx
@@ -1,12 +1,57 @@
 import React from "react";
 
+// styles
+import "./About.scss";
+
+// component
 type Props = {};
 
 const About = (props: Props) => {
   return (
     <div className="about">
       <h1>About</h1>
-      <p>about something</p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent et
+        neque non odio vulputate auctor. Nam non mi pulvinar, condimentum odio
+        sed, hendrerit ante. Mauris vehicula nunc eget mauris commodo, quis
+        vehicula massa tempor. Sed et arcu vitae metus vestibulum laoreet in vel
+        magna. Aenean tincidunt vestibulum lacus ac sollicitudin. Donec laoreet
+        a libero non rhoncus. Pellentesque tincidunt elit ac leo mattis, id
+        pretium est condimentum. Maecenas faucibus ante sem, vitae condimentum
+        odio luctus ut. Donec maximus iaculis velit et pellentesque. Maecenas
+        aliquet consectetur eros molestie auctor. Fusce non felis a arcu
+        condimentum ultricies sed at ligula. Interdum et malesuada fames ac ante
+        ipsum primis in faucibus. Integer lacinia facilisis diam vitae maximus.
+        Curabitur ultrices augue vel sodales efficitur. Aliquam at condimentum
+        justo. Nam leo nisi, sagittis eu lectus et, fermentum fringilla felis.
+        Pellentesque vitae placerat velit. Duis sit amet leo sagittis, finibus
+        nisi ut, porttitor diam. Vestibulum sit amet accumsan dui. Nullam
+        scelerisque sagittis dui, ac congue metus suscipit id. Morbi lacinia
+        semper neque. Curabitur hendrerit in ex vel aliquam. Vestibulum
+        scelerisque risus ac tellus gravida, et sollicitudin velit porta.
+        Vestibulum ac egestas justo. Fusce id justo a turpis tincidunt
+        malesuada. Fusce nisl leo, venenatis eu dictum a, pellentesque non nisl.
+        Mauris tincidunt turpis at lectus dapibus fringilla. Vestibulum ante
+        ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae;
+        Cras in aliquam mi. Curabitur eros turpis, scelerisque vitae eleifend
+        in, viverra ut tellus. Donec porta ut velit sit amet cursus.
+        Pellentesque mi neque, hendrerit et ex sed, imperdiet varius libero.
+        Nullam laoreet ante vel blandit dapibus. Nunc lacus quam, imperdiet quis
+        rutrum sit amet, mattis a ipsum. Etiam euismod, urna id malesuada
+        euismod, ligula dolor euismod mi, at convallis purus mauris et mauris.
+        Nam commodo ipsum id tortor iaculis, non commodo magna dignissim. In hac
+        habitasse platea dictumst. Donec a leo a velit feugiat dignissim. Ut
+        vulputate sem sit amet maximus dapibus. Etiam in mattis ante. Praesent
+        sem massa, tristique a semper in, suscipit elementum erat. Curabitur
+        rhoncus mollis interdum. Proin a ultricies nisl, eu dignissim sapien.
+        Nam erat elit, lacinia sed lacinia et, iaculis nec lacus. Pellentesque
+        pulvinar metus vel mauris porttitor, sed molestie lectus faucibus. Proin
+        tincidunt porttitor enim, at suscipit arcu pharetra non. Proin cursus a
+        magna nec faucibus. Sed finibus, nunc a porttitor scelerisque, est
+        sapien commodo leo, id malesuada ante lorem dictum libero. Sed
+        sollicitudin dui a scelerisque ultricies. Sed iaculis fringilla
+        tincidunt. Curabitur ultrices vel libero vel pellentesque.
+      </p>
     </div>
   );
 };
